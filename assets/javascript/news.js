3ea28487cf123357ecf54c510727462895ff7c2f
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-06-25 21:24:34 -0600

    notifications on api working

--- assets/javascript/news.js
+++ assets/javascript/news.js
@@ -1,3 +1,18 @@
-// https://rapidapi.com/mikilior1/api/FinancialTimes
-// https://rapidapi.com/ricardodegregorio/api/stock-news
-// https://rapidapi.com/zakutynsky/api/Blockchain
+
+
+// let api = "http://api.ft.com/content/search/v1?"
+let api = "https://api.ft.com/content/notifications?"
+let apiKey = "apiKey=59cbaf20e3e06d3565778e7bd4763621eb134dd6adf6fd8077539a9e"
+let query = "&since=2019-04-28T03:22:52.394Z"
+let queryURL = ("https://cors-anywhere.herokuapp.com/" + api + apiKey + query);
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response)
+}).catch(err => {
+    console.log(err)
+})
+
