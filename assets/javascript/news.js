6562ad158206d6ddd735effaf3ef9015271a476c
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-06-27 20:44:07 -0600

    Search button for News

--- assets/javascript/news.js
+++ assets/javascript/news.js
@@ -1,35 +1,40 @@
-//FINANCIAL TIMES FAIL
-/* // let api = "http://api.ft.com/content/search/v1?"
-let api = "https://api.ft.com/content/notifications?"
-let apiKey = "apiKey=59cbaf20e3e06d3565778e7bd4763621eb134dd6adf6fd8077539a9e"
-let query = "&since=2019-04-28T03:22:52.394Z"
-let queryURL = ("https://cors-anywhere.herokuapp.com/" + api + apiKey + query); */
-
 //NY TIMES
 //FORMAT: /articlesearch.json?q={query}&fq={filter}
 //ex: https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
 
-let api = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
-let query = "q=bitcoin&" //bitcoin will be input captured from Home page
-let apiKey = "api-key=n7kN1nsbvcCZ4ymY8aA7OAgd2dNWkzD3"
-let queryURL = (/* "https://cors-anywhere.herokuapp.com/" + */ api + query + apiKey); 
+$('#button').on("click",function(){
+    let cryptoSearch = document.getElementById("inputSearch").value    
+    let api = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
+    let query = `q=${cryptoSearch}&` //bitcoin will be input captured from Home page
+    let apiKey = "api-key=n7kN1nsbvcCZ4ymY8aA7OAgd2dNWkzD3"
+    let queryURL = (/* "https://cors-anywhere.herokuapp.com/" + */ api + query + apiKey); 
 
-for(let i =0; i<4; i++){
+    //Removes headlines, snippets, and news links h1s so adjust
+ /*  $('#headlines').empty();
+    $('#snippets').empty();
+    $('#newsLinks').empty(); */
+
+for(let i =0; i<2; i++){
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
-    let headline = "Headline: "+response.response.docs[i].headline.main;
+    let headline = response.response.docs[i].headline.main;
     let articleUrl = response.response.docs[i].web_url;
     let link = articleUrl.link(articleUrl); //turning URLs to clickable links for user
+    let snippet =  response.response.docs[i].snippet;
 
-    $('#newsLinks').append(headline);
+    $('#headlines').append(i+1 +": "+ headline);
+    $('#headlines').append("<p></p>");
+    $('#snippets').append(i+1 +": "+ snippet);
+    $('#snippets').append("<p></p>");
+    $('#newsLinks').append(i+1 +": "+ link); 
     $('#newsLinks').append("<p></p>");
-    $('#newsLinks').append(link); 
-    $('#newsLinks').append("<p></p>");
-     console.log(response);
 })
 }
+})
+
+
 
 
 
